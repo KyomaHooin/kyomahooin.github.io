f9a21c0970f025b55ad17eb73a9c03d659ef930e
KyomaHooin <kyomahooin@gmail.com>
2023-05-16 10:33:18 +0200

    π^4

--- custom.js
+++ custom.js
@@ -1,3 +1,7 @@
 //π
-function pi() { window.location.href = "/2fa"; }
+var p = false;
+document.addEventListener('keydown', (e) => { if (e.shiftKey && e.ctrlKey ) { p = true; }});
+document.addEventListener('keyup', (e) => { if (e.shiftKey || e.ctrlKey ) { p = false; }});
+
+function pi() { if(p) { window.location.href = "/2fa"; }}
 
