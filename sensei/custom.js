1c399ebf096fe7cb856096695b8358b489055cc1
KyomaHooin <kyomahooin@gmail.com>
2024-02-14 20:34:07 +0100

    桜

--- sensei/custom.js
+++ sensei/custom.js
@@ -1,12 +1,59 @@
 
+// TOKEN
+
+const url = 'https://api.github.com/repos/kyomahooin/Sakura/contents/';
+const token_base = 'Z2l0aHViX3BhdF8xMUFFU01KTVkwdFZUbkRscldNaEtSXw==';
+var file = '';
+
+// API GET CONTENT
+
+async function get_content(fn, token) {
+	console.error(url + fn);
+	console.error(token_base + token);
+	//return await fetch(url + fn, {
+	return await fetch(url + 'README.md', {
+		method: 'GET',
+		headers: {
+			'Authorization':'Bearer ' + atob(token_base) + token,
+			'Accept':'application/vnd.github.raw'
+		}
+	})
+	.then(response => {
+		if (response.ok) {
+			return response.blob();
+		}
+		return false;
+	})
+	.catch(error => {
+		console.error(error);
+		return false;
+	});
+}
+
 // MODAL
 
 modal = new bootstrap.Modal(document.getElementById('modal'));
 
-function image_modal(img) {
-	document.getElementById('modal-image').src = img;
+function mod(fn) {
 	modal.toggle();
+	file = fn;
 }
 
-function image_close() { modal.toggle();}
+async function on_confirm() {
+	token = document.getElementById('modal-token').value;
+	const ret = await get_content(file, token);
+	if (!ret) {
+		document.getElementById('modal-token').value = '失敗しました';
+	} else {
+		modal.toggle();
+		var a = document.createElement("a");
+		a.style = "display: none";
+		document.body.appendChild(a);
+		var f = window.URL.createObjectURL(ret);
+		a.href = f;
+		a.download = file;
+		a.click();
+		window.URL.revokeObjectURL(f);
+	}
+}
 
